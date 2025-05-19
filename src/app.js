3f7dd70c8644e786852f13cf318ed86ebe4bbbd7
Raushan Raj <kumarraushanraj10@gmail.com>
2025-05-19 17:07:31 +0530

    basic2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express =require("express")
  
 const app =express();
-app.use((req,res)=>{
-    res.send("server started")
+
+app.get("/getting",(req,res)=>{
+    const userobj={
+        firsrName:"Raushan",
+        lastName:"Raj",
+        age:25
+    }
+    res.send(userobj)
 })
+
+
+
 
 app.listen(3333,()=>{console.log("server is listen now")})
