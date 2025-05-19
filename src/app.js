54fd073b5ae1f9fb463a403810212f55ed34e906
Raushan Raj <kumarraushanraj10@gmail.com>
2025-05-19 17:43:18 +0530

    next()

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express =require("express")
  
 const app =express();
 
-app.get("/user",(req,res)=>{
+app.get("/user",(req,res,next)=>{
    const id= req.params
     console.log(req.query)
+        next();
     res.send("hi there")
+        //  next();
+},(req,res)=>{
+    console.log("dhf")
+    res.send("hdfshf")
 })
 
 // const userobj={
