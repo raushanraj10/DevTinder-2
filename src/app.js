8f5390726474d60512013a12c9ded1d94bb6335c
Raushan Raj <kumarraushanraj10@gmail.com>
2025-05-19 17:36:51 +0530

    params and query

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ const express =require("express")
  
 const app =express();
 
-app.get("/getting",(req,res)=>{
-    const userobj={
-        firsrName:"Raushan",
-        lastName:"Raj",
-        age:25
-    }
-    res.send(userobj)
+app.get("/user",(req,res)=>{
+   const id= req.params
+    console.log(req.query)
+    res.send("hi there")
 })
 
-
+// const userobj={
+    //     firsrName:"Raushan",
+    //     lastName:"Raj",
+    //     age:25
+    // }
 
 
 app.listen(3333,()=>{console.log("server is listen now")})
